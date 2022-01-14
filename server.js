99eeea2bd4efc143ec9932800c2206fcbc774152
[Tomas] <btomasmarquez@gmail.com>
2022-01-14 01:26:19 -0300

    form post in process

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require("body-parser")
+const { check, validationResult } = require("express-validator");
+const { request } = require('http');
 const app = express()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(express.static(__dirname, 'index.html'))
+})
+
+app.get('/contact-me', function (request, response){
+  return response.render('contact-me')
+})
+
+app.post('/contact-me', urlencodedParser, (request, response) =>{
+  response.json(request.body)
 })
 
 
